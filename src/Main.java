a9ec1f78ef4c1dd8c289617778ee7d3886b6abd6
Yuri <marcos.yuriro@gmail.com>
2025-09-23 15:21:34 -0300

    feat(Main): Initial Structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Você inicia mais uma rotina de trabalho. Ao entrar em sua cabine" +
+                "e organizar sua mesa, você clica o alarme para a análise do primeiro imigrante" +
+                "que possivelmente poderá adentrar em seu país.");
+
+        System.out.println("""
+                Você inicia mais uma rotina de trabalho. Ao entrar em sua cabine
+                e organizar sua mesa, você clica o alarme para a análise do primeiro imigrante
+                que possivelmente poderá adentrar em seu país.""");
+
+        //Após isso, já pode haver um metodo de exibição/introdução do imigrante junto de uma lista de ações
+
 
     }
 }
