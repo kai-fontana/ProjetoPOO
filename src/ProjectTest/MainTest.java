a5cff1ef1dcab1be237ad6cd224dde7c30b06d64
Marcos <marcos.yuriro@gmail.com>
2025-09-24 08:24:27 -0300

    feat(ProjectTest) code addiction in MainTest

--- src/ProjectTest/MainTest.java
+++ src/ProjectTest/MainTest.java
@@ -1,4 +1,14 @@
 package ProjectTest;
 
 public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("""
+                Você inicia mais uma rotina de trabalho. Ao entrar em sua cabine
+                e organizar sua mesa, você clica o alarme para a análise do primeiro imigrante
+                que possivelmente poderá adentrar em seu país.""");
+
+        //Após isso, já pode haver um metodo de exibição/introdução do imigrante junto de uma lista de ações
+
+
+    }
 }
